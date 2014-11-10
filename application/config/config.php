e153ef0390a56ccf4fe623d8687651c78d7f79db
Deniel <d_niel@live.com>
2014-11-10 14:11:58 +0700

    tes config gtignore

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/gss';
+$config['base_url']	= 'http://localhost/gsss';
 
 /*
 |--------------------------------------------------------------------------
