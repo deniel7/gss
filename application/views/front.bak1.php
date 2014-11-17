bf86f9fb1504dd874a27fb85e1516606d74d6fff
Deniel <d_niel@live.com>
2014-11-17 13:09:48 +0700

    Revert "Revert bb4139c..865fcf2"
    This reverts commit f0841ef531edae42326dcad15d9f85e245c5f18c.

---
+++ application/views/front.bak1.php
@@ -0,0 +1,163 @@
+<p>
+<?php //echo @$sambutan ?>
+</p>
+
+<div style="height:500px">
+    <span>
+        <div style="border:solid thin; height:30px; background-image: url('images/h3bg.png'); background-repeat:no-repeat; background-position:right; background-color:#E61A13; border:none;  margin-bottom:20px;">
+<span style="font-size:16px; color:white;">FASHION</span></div>
+    <div class="produk-wrap-front">
+        <div class="produk-name">
+            MENS
+        </div>
+        <div class="image-wrap">
+            <div class="image-iner">
+                
+                <a href="<?php echo base_url()."index.php/produk/mens"; ?>"><img src="<?php echo base_url()."images/category/mens.jpg"; ?>" /></a>
+                
+            </div>
+        </div>
+    </div>
+    </span>
+    <span>
+    <div class="produk-wrap-front">
+        <div class="produk-name">
+            WOMANS
+        </div>
+        <div class="image-wrap">
+            <div class="image-iner">
+                
+                    <a href="<?php echo base_url()."index.php/produk/womans"; ?>"><img src="<?php echo base_url()."images/category/womens.jpg"; ?>" /></a>
+                
+            </div>
+        </div>
+    </div>
+    </span>
+    
+    <span>
+    <div class="produk-wrap-front">
+        <div class="produk-name">
+            BABY & KIDS
+        </div>
+        <div class="image-wrap">
+            <div class="image-iner">
+               
+                    <a href="<?php echo base_url()."index.php/produk/baby_and_kids"; ?>"><img src="<?php echo base_url()."images/category/baby.jpg"; ?>" /></a>
+                
+            </div>
+        </div>
+    </div>
+    </span>
+    
+    <div>
+        <span>
+        <div class="produk-wrap-front">
+        <div class="produk-name">
+            SHOES & BAGS
+        </div>
+        <div class="image-wrap">
+            <div class="image-iner">
+                
+                    <a href="<?php echo base_url()."index.php/produk/shoes_and_bags"; ?>"><img src="<?php echo base_url()."images/category/bags.jpg"; ?>" /></a>
+                
+            </div>
+        </div>
+        </div>
+        </span>
+        
+        <span>
+        <div class="produk-wrap-front">
+        <div class="produk-name">
+            BEAUTY & ACCESORIES
+        </div>
+        <div class="image-wrap">
+            <div class="image-iner">
+                
+                    <a href="<?php echo base_url()."index.php/produk/beauty_and_accesories"; ?>"><img src="<?php echo base_url()."images/category/acc.jpg"; ?>" /></a>
+                
+            </div>
+        </div>
+        </div>
+        </span>
+    </div>
+</div>
+
+<div style="height:500px">
+    <span>
+        <div style="border:solid thin; height:30px; background-image: url('images/h3bg.png'); background-repeat:no-repeat; background-position:right; background-color:#E61A13; border:none; margin-bottom:20px">
+<span style="font-size:16px; color:white; ali">SUPERMARKET</span></div>
+    <div class="produk-wrap-front">
+        <div class="produk-name">
+            FOOD
+        </div>
+        <div class="image-wrap">
+            <div class="image-iner">
+                
+                    <a href="<?php echo base_url()."index.php/produk/food"; ?>"><img src="<?php echo base_url()."images/category/food.jpg"; ?>" /></a>
+                
+            </div>
+        </div>
+    </div>
+    </span>
+    <span>
+    <div class="produk-wrap-front">
+        <div class="produk-name">
+            NON FOOD
+        </div>
+        <div class="image-wrap">
+            <div class="image-iner">
+                
+                    <a href="<?php echo base_url()."index.php/produk/non_food"; ?>"><img src="<?php echo base_url()."images/category/nonfood.jpg"; ?>" /></a>
+                
+            </div>
+        </div>
+    </div>
+    </span>
+    
+    <span>
+    <div class="produk-wrap-front">
+        <div class="produk-name">
+            GMS
+        </div>
+        <div class="image-wrap">
+            <div class="image-iner">
+              
+                    <a href="<?php echo base_url()."index.php/produk/gms"; ?>"><img src="<?php echo base_url()."images/category/gms.jpg"; ?>" /></a>
+                
+            </div>
+        </div>
+    </div>
+    </span>
+    
+    <div>
+        <span>
+        <div class="produk-wrap-front">
+        <div class="produk-name">
+            FRESH
+        </div>
+        <div class="image-wrap">
+            <div class="image-iner">
+                
+                    <a href="<?php echo base_url()."index.php/produk/fresh"; ?>"><img src="<?php echo base_url()."images/category/fresh.jpg"; ?>" /></a>
+                
+            </div>
+        </div>
+        </div>
+        </span>
+        
+        <span>
+        <div class="produk-wrap-front">
+        <div class="produk-name">
+            BAKERY
+        </div>
+        <div class="image-wrap">
+            <div class="image-iner">
+                
+                    <a href="<?php echo base_url()."index.php/produk/bakery"; ?>"><img src="<?php echo base_url()."images/category/bakery.jpg"; ?>" /></a>
+                
+            </div>
+        </div>
+        </div>
+        </span>
+    </div>
+</div>
